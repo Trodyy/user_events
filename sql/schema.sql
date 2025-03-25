16d9dd6b1f14df3fb12c9d9db623da552f74ec9a
trody <mahdi.karimi.beirnavand2003@gmail.com>
2025-03-25 14:17:46 +0330

    feat : Create user_metrics table and calculate_avg_time() function.

--- sql/schema.sql
+++ sql/schema.sql
@@ -21,3 +21,31 @@ CREATE VIEW user_activity_summary AS
 SELECT user_id, COUNT(*) AS visit_count, AVG(duration) AS avg_time
 FROM user_logs
 GROUP BY user_id;
+
+
+
+//MATRILIZIED VIEWS HAVE BETTER PERFORMANCE WHERE WE HAVE COMPLEX aGGREGATIONS THAT ARE EXPENSIVE TO COMPUTE.
+CREATE MATERIALIZED VIEW daily_page_views AS
+SELECT DATE(timestamp) AS date, page, COUNT(*) AS visits
+FROM user_logs
+GROUP BY DATE(timestamp), page;
+
+
+
+//Create a new table to get the output of trigger.
+CREATE TABLE user_metrics (
+    user_id INT PRIMARY KEY,
+    avg_time FLOAT
+);
+
+
+//CREATE A FUNCTION TO COMUTE avg_time and INSERT it on user_metrics.
+CREATE OR REPLACE FUNCTION calculate_avg_time() RETURNS VOID AS $$
+BEGIN
+    INSERT INTO user_metrics (user_id, avg_time)
+    SELECT user_id, AVG(duration)
+    FROM user_logs
+    GROUP BY user_id
+    ON CONFLICT (user_id) DO UPDATE SET avg_time = EXCLUDED.avg_time;
+END;
+$$ LANGUAGE plpgsql;
